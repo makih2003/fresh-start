027cc84399df3b1cb2a3b4ca00539b3a6ecf7a12
Maki <hm14230024@icloud.com>
2023-10-21 12:17:58 -0700

    start first page.css

--- fresh-start-frontend/src/components/FirstPage.js
+++ fresh-start-frontend/src/components/FirstPage.js
@@ -1,11 +1,13 @@
+import "./FirstPage.css";
+
 function FirstPage({ onHandleSubmit }) {
   const nextYear = new Date().getFullYear() + 1;
 
   return (
-    <div>
+    <div className="content">
       <h1>New Year, New Me! </h1>
-      <form onSubmit={onHandleSubmit}>
-        <h2>What would you like to do more in {nextYear}</h2>
+      <h2>What would you like to do more in {nextYear} ?</h2>
+      <form className="user-form" onSubmit={onHandleSubmit}>
         <input type="text" placeholder="Your new year resolution..."></input>
         <button>Submit</button>
       </form>
