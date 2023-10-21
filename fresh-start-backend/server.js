aac3e6a22bb47495a36577b434ec0a094ffb3aeb
miguel-fg <miguelfierro55@gmail.com>
2023-10-21 10:59:40 -0700

    added db connection and listener for requests

--- fresh-start-backend/server.js
+++ fresh-start-backend/server.js
@@ -1,3 +1,35 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const resolutionRoutes = require("./routes/resolutions");
 
 const app = express();
+app.use(cors());
+
+const port = process.env.PORT;
+
+// log requests
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// access json data
+app.use(express.json());
+
+// routes
+app.use("/api/resolutions", resolutionRoutes);
+
+// connect to DB
+mongoose.set("strictQuery", false);
+const mongodb = process.env.MONGO_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+    if (port) {
+        app.listen(port, () => {
+            console.log(`connected to DB & listening on port ${port}`);
+        })
+    }
+}
