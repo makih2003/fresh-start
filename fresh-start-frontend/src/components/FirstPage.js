6bfca93235e0b32767e4b077982055f913822b6b
Maki <hm14230024@icloud.com>
2023-10-21 17:12:06 -0700

    finished css animation

--- fresh-start-frontend/src/components/FirstPage.js
+++ fresh-start-frontend/src/components/FirstPage.js
@@ -1,6 +1,6 @@
 import "./FirstPage.css";
 
-function FirstPage({ onHandleSubmit, newRes, setNewRes }) {
+function FirstPage({ onHandleSubmit, newRes, setNewRes, isOffensive }) {
   const nextYear = new Date().getFullYear() + 1;
 
   return (
@@ -8,9 +8,22 @@ function FirstPage({ onHandleSubmit, newRes, setNewRes }) {
       <h1>New Year, New Me! </h1>
       <h2>What would you like to do more in {nextYear} ?</h2>
       <form className="user-form" onSubmit={onHandleSubmit}>
-        <input type="text" placeholder="Your new year resolution..." value={newRes} onChange={(e) => {setNewRes(e.target.value)}}></input>
+        <input
+          type="text"
+          placeholder="Your new year resolution..."
+          value={newRes}
+          onChange={(e) => {
+            setNewRes(e.target.value);
+          }}
+        ></input>
         <button>Submit</button>
       </form>
+
+      {isOffensive ? (
+        <p className="message">Please keep the message clean :D</p>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
